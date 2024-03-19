e585f89c541d839bee627057e08d1906d8f73470
waltscode <walterswork91@outlook.com>
2024-03-18 21:42:17 -0400

    added three.js logic and a functioning animation that i worked on and it works

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
+import MyThree from '../components/comps/three'
 
 export function Contact() {
 
     return (
         <div id="contact">
-            <h1>Contact Me</h1>
-            <p>You can reach me at:</p>
-            <canvas id="canvas1"></canvas>
+            
+            
+            <MyThree />
         </div>
     );
 }
