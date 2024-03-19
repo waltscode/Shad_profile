1a5c162631177821c6f4d09e00ed2bd818b9c921
waltscode <walterswork91@outlook.com>
2024-03-19 16:29:01 -0400

    homepage is finally finished looking acceptable enough

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -74,37 +74,37 @@ export default function About() {
                 <div className="flex justify-center items-center mt-4">
                     <img src="/images/blue2.png" alt="profile picture" className="blob-effect" />
                 </div>
-                
-                    <Card className="text-left m-4 flex-grow h-80">
-                        <CardHeader>
-                            <CardTitle>John Walters</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <h2>Welcome to my portfolio website!</h2>
-                            <p>I am a novice full-stack web developer who recently completed a comprehensive full-stack web development bootcamp at the University of Toronto. With a rich background in IT spanning several years, technology is my passion and I love to learn new systems. I have recently found that I quite enjoy coding and it's my ambition to collaborate building new apps and really hone my skills in the field.</p>
-                        </CardContent>
-                    </Card>
-                
-                
-                    <Card className="text-left m-5 flex-grow h-80">
-                        <CardHeader>
-                            <CardTitle>Technology Used On This App</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <ul className="list-disc">
-                                <li>React.js</li>
-                                <li>Tailwind CSS</li>
-                                <li>Shadow DOM (Shadcn)</li>
-                                <li>Three.js</li>
-                                <li>CSS3</li>
-                                <li>HTML5</li>
-                                <li>Vite</li>
-                                <li>Node.js</li>
-                                <li>JavaScript</li>
-                            </ul>
-                        </CardContent>
-                    </Card>
-                
+
+                <Card className="text-left m-4 flex-grow h-80 mt-10">
+                    <CardHeader>
+                        <CardTitle>John Walters</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <h2>Welcome to my portfolio website!</h2>
+                        <p>I am a novice full-stack web developer who recently completed a comprehensive full-stack web development bootcamp at the University of Toronto. With a rich background in IT spanning several years, technology is my passion and I love to learn new systems. I have recently found that I quite enjoy coding and it's my ambition to collaborate building new apps and really hone my skills in the field.</p>
+                    </CardContent>
+                </Card>
+
+
+                <Card className="text-left m-5 flex-grow h-80 mt-10">
+                    <CardHeader>
+                        <CardTitle>Technology Used On This App</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <ul className="list-disc">
+                            <li>React.js</li>
+                            <li>Tailwind CSS</li>
+                            <li>Shadow DOM (Shadcn)</li>
+                            <li>Three.js</li>
+                            <li>CSS3</li>
+                            <li>HTML5</li>
+                            <li>Vite</li>
+                            <li>Node.js</li>
+                            <li>JavaScript</li>
+                        </ul>
+                    </CardContent>
+                </Card>
+
             </div>
 
 
@@ -158,7 +158,7 @@ export default function About() {
 
             <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 mt-8"> {/* Added mt-8 for top margin */}
                 <div className="flex justify-center items-center">
-                    <Card className="hiding m-4 text-left w-full sm:w-auto md:w-2/3 lg:w-2/3 xl:w-2/3">
+                    <Card className="hiding2 m-4 text-left w-full sm:w-auto md:w-2/3 lg:w-2/3 xl:w-2/3">
                         <CardHeader>
                             <CardTitle>Back End Technologies</CardTitle>
                         </CardHeader>
