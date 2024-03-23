339bf5756b1be233f61c642ef36539a64a98e105
waltscode <walterswork91@outlook.com>
2024-03-23 01:04:38 -0400

    toggle was buggy on phone, moved it

--- src/components/comps/navbar.jsx
+++ src/components/comps/navbar.jsx
@@ -41,8 +41,8 @@ export function Navbar() {
                                 </Disclosure.Button>
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex flex-shrink-0 items-center  text-gray-300 px-3 py-2 text-sm font-medium">
-                                    
+                                <div className="flex flex-shrink-0 items-center   px-3 py-2 text-sm font-medium">
+                                <ModeToggle />
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
@@ -67,7 +67,7 @@ export function Navbar() {
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
 
-                                <ModeToggle />
+                                
 
 
                                 <DrawerDemo />
